197014effc0e821811f6fe2dd0cceba156547c83
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-03-18 12:29:42 +0100

    Add file store.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 
 import App from './App';
 
@@ -9,7 +12,11 @@ import "font-awesome/css/font-awesome.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
 
