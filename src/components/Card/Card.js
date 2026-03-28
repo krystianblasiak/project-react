b091ebc6a56d116cd600c3c0e454c1c2a33c3efb
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-03-28 06:31:34 +0100

    Change component Card and file cardsRedux.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,31 @@
 import { useDispatch } from "react-redux";
-import styles from "./Card.module.scss";
-import { toggleCardFavorite } from "../../redux/subreducery/cardsRedux";
+
+import { toggleCardFavorite, removeCard } from "../../redux/subreducery/cardsRedux";
+
 import clsx from "clsx";
+import styles from "./Card.module.scss";
 
 
 const Card = props => {
     const dispatch = useDispatch();
     const cardId = props.cardId;
 
-    const handlerClick = e => {
+    const handlerStar = e => {
         e.preventDefault();
         dispatch(toggleCardFavorite(cardId));
     };
 
+    const handlerTrash = e => {
+        e.preventDefault();
+        dispatch(removeCard(cardId));
+    };
+
     return (
         <li className={styles.card}>{props.title}
-            <button className={"fa fa-star-o " + styles.btnStar + " " + clsx(props.isFavorite === true && styles.active)} onClick={handlerClick}></button>
-
+            <div className={styles.div}>
+                <button className={"fa fa-star-o " + styles.btnStar + " " + clsx(props.isFavorite === true && styles.active)} onClick={handlerStar} />
+                <button className={"fa fa-trash " + styles.btnTrash} onClick={handlerTrash} />
+            </div>
         </li>
     );
 };
--- src/redux/subreducery/cardsRedux.js
+++ src/redux/subreducery/cardsRedux.js
@@ -9,10 +9,12 @@ export const getFilteredCardsIsFavorite = ({ cards }) => cards.filter(card => ca
 const createActionName = actionName => `app/cards/${actionName}`;
 const ADD_CARD = createActionName("ADD_CARD");
 const TOGGLE_CARD_FAVORITE = createActionName("TOGGLE_CARD_FAVORITE");
+const REMOVE_CARD = createActionName("REMOVE_CARD");
 
 // actions creators
 export const addCard = payload => ({ type: ADD_CARD, payload });
-export const toggleCardFavorite = payload => ({ type: TOGGLE_CARD_FAVORITE, payload});
+export const toggleCardFavorite = payload => ({ type: TOGGLE_CARD_FAVORITE, payload });
+export const removeCard = payload => ({ type: REMOVE_CARD, payload });
 
 const cardsReducer = (statePart = [], action) => {
     switch(action.type){
@@ -20,6 +22,8 @@ const cardsReducer = (statePart = [], action) => {
             return [ ...statePart, { ...action.payload, id: shortid() }];
         case TOGGLE_CARD_FAVORITE:
             return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+        case REMOVE_CARD:
+            return statePart.filter(card => card.id !== action.payload);
         default:
             return statePart;
     }
